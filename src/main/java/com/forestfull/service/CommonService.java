4765999253a929b9ac90f72490e493376d810bdf
Xasquatch <bigfoot7774@gmail.com>
2024-06-09 18:50:03 +0900

    init

---
+++ src/main/java/com/forestfull/service/CommonService.java
@@ -0,0 +1,15 @@
+package com.forestfull.service;
+
+import com.forestfull.mapper.CommonMapper;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class CommonService {
+
+    private final CommonMapper commonMapper;
+
+
+
+}
---
+++ src/main/java/com/forestfull/util/someoneUtil.java
@@ -0,0 +1,4 @@
+package com.forestfull.util;
+
+public class someoneUtil {
+}
