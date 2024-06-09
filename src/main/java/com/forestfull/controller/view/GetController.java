ebceb245a5f7dc4892e87fd18cf0e7ef8616f85e
Xasquatch <bigfoot7774@gmail.com>
2024-06-09 19:09:10 +0900

    todolist - security + jwt - mybatis - stateless, oauth kakao, websocket

---
+++ src/main/java/com/forestfull/controller/view/GetController.java
@@ -0,0 +1,11 @@
+package com.forestfull.controller.view;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+
+@Controller
+@RequiredArgsConstructor
+public class GetController {
+
+
+}
