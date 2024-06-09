483c01c899ca5299c86a4365e55eb2ffa2996854
Xasquatch <bigfoot7774@gmail.com>
2024-06-09 19:28:42 +0900

    init
    TODO LIST
    - security + jwt
    - mybatis
    - stateless, oauth kakao, websocket
    - P&G

---
+++ src/main/java/com/forestfull/config/JasyptConfig.java
@@ -0,0 +1,40 @@
+package com.forestfull.config;
+
+import org.jasypt.encryption.StringEncryptor;
+import org.jasypt.encryption.pbe.PooledPBEStringEncryptor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
+
+import java.util.function.BiConsumer;
+
+@Component
+public class JasyptConfig {
+
+    private static class EncryptionSetting {
+        public static void main(String[] args) {
+
+        }
+
+        private static StringEncryptor initEncryptor(String key) {
+            final PooledPBEStringEncryptor encryptor = new PooledPBEStringEncryptor();
+            encryptor.setPassword(key);
+            encryptor.setKeyObtentionIterations(1000);
+            encryptor.setPoolSize(1);
+            return encryptor;
+        }
+
+        private static final BiConsumer<String, String> enc = (key, str)
+                -> System.out.println(str + " : ENC(" + initEncryptor(key).encrypt(str) + ")");
+        private static final BiConsumer<String, String> dec = (key, str)
+                -> System.out.println(str + " : " + initEncryptor(key).decrypt(str));
+    }
+
+    @Value("${spring.profiles.active}")
+    private String key;
+
+    @Bean("jasyptStringEncryptor")
+    StringEncryptor stringEncryptor() {
+        return EncryptionSetting.initEncryptor(key);
+    }
+}
