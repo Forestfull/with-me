699dc23d87ce65eb2ab8d19c29d28a7bbd52b27b
Xasquatch <bigfoot7774@gmail.com>
2024-06-09 18:35:47 +0900

    Initial commit

---
+++ src/test/java/com/forestfull/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.forestfull;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WithMeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
