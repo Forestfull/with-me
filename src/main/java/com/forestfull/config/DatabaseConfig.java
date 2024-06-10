91846ab06c793338aa522e0598c7bfa28ceb4cc7
Xasquatch <bigfoot7774@gmail.com>
2024-06-10 17:18:23 +0900

    mybatis config
    TODO LIST
    - security + jwt
    - mybatis
    - stateless, oauth kakao, websocket
    - P&G

---
+++ src/main/java/com/forestfull/config/DatabaseConfig.java
@@ -0,0 +1,36 @@
+package com.forestfull.config;
+
+import lombok.RequiredArgsConstructor;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.mybatis.spring.SqlSessionFactoryBean;
+import org.mybatis.spring.SqlSessionTemplate;
+import org.mybatis.spring.boot.autoconfigure.SpringBootVFS;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
+import java.io.IOException;
+
+@Configuration
+@RequiredArgsConstructor
+public class DatabaseConfig {
+
+    private final ApplicationContext applicationContext;
+
+    @Bean
+    public SqlSessionFactory sqlSessionFactory(DataSource dataSource) throws Exception {
+        SqlSessionFactoryBean sqlSessionFactory = new SqlSessionFactoryBean();
+        sqlSessionFactory.setDataSource(dataSource);
+        sqlSessionFactory.setVfs(SpringBootVFS.class);
+        sqlSessionFactory.setMapperLocations(applicationContext.getResources("classpath:mapper/**/*.xml"));
+        sqlSessionFactory.setConfigLocation(applicationContext.getResource("classpath:mybatis-config.xml"));
+        return sqlSessionFactory.getObject();
+    }
+
+    @Bean
+    public SqlSessionTemplate sqlSessionTemplate(SqlSessionFactory sqlSessionFactory) {
+        return new SqlSessionTemplate(sqlSessionFactory);
+    }
+}
