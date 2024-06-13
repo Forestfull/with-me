b121721e7df0d0de378ad58a92b2b472ef8f342d
Xasquatch <bigfoot7774@gmail.com>
2024-06-13 17:31:43 +0900

    https://sjh9708.tistory.com/170 나랑 같은 환경이 있어서 퍼옴
    TODO LIST
    - security + jwt
    - mybatis
    - stateless, oauth kakao, websocket
    - P&G

--- src/main/java/com/forestfull/config/SecurityConfig.java
+++ src/main/java/com/forestfull/config/SecurityConfig.java
@@ -1,26 +1,25 @@
-package com.forestfull.config;
+    package com.forestfull.config;
 
-import io.jsonwebtoken.Jwts;
-import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.*;
+import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.InitializingBean;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.stereotype.Component;
 
 import java.security.Key;
-import java.util.Base64;
+import java.time.Clock;
+import java.time.LocalDateTime;
 import java.util.Date;
-import java.util.stream.Collectors;
 
 @Configuration
 @EnableWebSecurity
@@ -38,5 +37,107 @@ public class SecurityConfig {
                 .csrf(AbstractHttpConfigurer::disable)
                 .sessionManagement(AbstractHttpConfigurer::disable)
                 .build();
+    }
+
+    @Slf4j
+    @Component
+    public static class JwtUtil {
+
+        private final Key key;
+        private final long accessTokenExpTime;
+
+        public JwtUtil(
+                @Value("${jwt.secret}") String secretKey,
+                @Value("${jwt.interval.seconds}") long accessTokenExpTime
+        ) {
+            byte[] keyBytes = Decoders.BASE64.decode(secretKey);
+            this.key = Keys.hmacShaKeyFor(keyBytes);
+            this.accessTokenExpTime = accessTokenExpTime;
+        }
+
+        /**
+         * Access Token 생성
+         *
+         * @param member
+         * @return Access Token String
+         */
+        public String createAccessToken(CustomUserInfoDto member) {
+            return createToken(member, accessTokenExpTime);
+        }
+
+
+        /**
+         * JWT 생성
+         *
+         * @param member
+         * @param expireTime
+         * @return JWT String
+         */
+        private String createToken(CustomUserInfoDto member, long expireTime) {
+            Claims claims = Jwts.claims();
+            claims.put("memberId", member.getMemberId());
+            claims.put("email", member.getEmail());
+            claims.put("role", member.getRole());
+
+            LocalDateTime now = LocalDateTime.now(Clock.systemUTC());
+            LocalDateTime tokenValidity = now.plusSeconds(expireTime);
+
+
+            return Jwts.builder()
+                    .setClaims(claims)
+                    .setIssuedAt(Date.from(now.toInstant()))
+                    .setExpiration(Date.from(tokenValidity.toInstant()))
+                    .signWith(key, SignatureAlgorithm.HS256)
+                    .compact();
+        }
+
+
+        /**
+         * Token에서 User ID 추출
+         *
+         * @param token
+         * @return User ID
+         */
+        public Long getUserId(String token) {
+            return parseClaims(token).get("memberId", Long.class);
+        }
+
+
+        /**
+         * JWT 검증
+         *
+         * @param token
+         * @return IsValidate
+         */
+        public boolean validateToken(String token) {
+            try {
+                Jwts.parserBuilder().setSigningKey(key).build().parseClaimsJws(token);
+                return true;
+            } catch (io.jsonwebtoken.security.SecurityException | MalformedJwtException e) {
+                log.info("Invalid JWT Token", e);
+            } catch (ExpiredJwtException e) {
+                log.info("Expired JWT Token", e);
+            } catch (UnsupportedJwtException e) {
+                log.info("Unsupported JWT Token", e);
+            } catch (IllegalArgumentException e) {
+                log.info("JWT claims string is empty.", e);
+            }
+            return false;
+        }
+
+
+        /**
+         * JWT Claims 추출
+         *
+         * @param accessToken
+         * @return JWT Claims
+         */
+        public Claims parseClaims(String accessToken) {
+            try {
+                return Jwts.parserBuilder().setSigningKey(key).build().parseClaimsJws(accessToken).getBody();
+            } catch (ExpiredJwtException e) {
+                return e.getClaims();
+            }
+        }
     }
 }
