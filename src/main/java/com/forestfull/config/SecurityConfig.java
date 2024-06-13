bdf92bd1d50276d8b50965090fb8607f23a76a0a
Xasquatch <bigfoot7774@gmail.com>
2024-06-13 17:19:59 +0900

    jwt 수정 (굳이?)
    TODO LIST
    - security + jwt
    - mybatis
    - stateless, oauth kakao, websocket
    - P&G

--- src/main/java/com/forestfull/config/SecurityConfig.java
+++ src/main/java/com/forestfull/config/SecurityConfig.java
@@ -1,19 +1,42 @@
 package com.forestfull.config;
 
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+
+import java.security.Key;
+import java.util.Base64;
+import java.util.Date;
+import java.util.stream.Collectors;
 
 @Configuration
 @EnableWebSecurity
 @RequiredArgsConstructor
 public class SecurityConfig {
 
-  	@Bean
-  	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-  		return http.build();
-  	}
+    @Bean
+    PasswordEncoder passwordEncoder() {
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
+
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http
+                .csrf(AbstractHttpConfigurer::disable)
+                .sessionManagement(AbstractHttpConfigurer::disable)
+                .build();
+    }
 }
