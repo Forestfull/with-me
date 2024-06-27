2a2255099417182423b0fc345d98b8e4d3195b1a
Xasquatch <bigfoot7774@gmail.com>
2024-06-27 20:05:10 +0900

    리다이렉트 uri 수정
    TODO LIST
    - (구성 중단) security + jwt
    - (구성 완료) mybatis
    - (진행중) stateless, oauth kakao
    - websocket
    - P&G

--- src/main/java/com/forestfull/controller/view/GetController.java
+++ src/main/java/com/forestfull/controller/view/GetController.java
@@ -1,18 +1,34 @@
 package com.forestfull.controller.view;
 
 import com.forestfull.service.SecureService;
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.Iterator;
 
 @Controller
 @RequiredArgsConstructor
 public class GetController {
 
     @GetMapping("/login")
-    String login(){
+    String login() {
         return "login";
+    }
+
+    @GetMapping("/login/kakao")
+    String loginViaKakao(HttpServletRequest request) {
+
+        final Iterator<String> iterator = request.getAttributeNames().asIterator();
+        while (iterator.hasNext()) {
+            System.out.println(
+                    request.getAttribute(iterator.next())
+            );
+        }
+
+        return "hi";
     }
 
 }
